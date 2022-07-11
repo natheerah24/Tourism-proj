773c28c8fe747ae3368f3b8cf02acdcc7de0c790
Raaid-Latief <raaidlatiefgbr@gmail.com>
2022-07-11 15:02:47 +0200

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AdminList from '../views/AdminList.vue';
+
+
 
 const routes = [
   {
@@ -14,6 +17,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: "/admin",
+    name: "admin-list",
+    component: AdminList,
+
   }
 ]
 
---
+++ src/views/AdminList.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-container>
+        <h1>Admin List</h1>
+
+        <div>
+            <table class="table table table-success table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+        
+        </div>
+        
+    </v-container>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
