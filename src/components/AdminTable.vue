4b60eb3682f5faa341c14ed2ea92753797fcf246
Raaid-Latief <raaidlatiefgbr@gmail.com>
2022-07-14 16:29:59 +0200

    update fix

--- src/components/AdminTable.vue
+++ src/components/AdminTable.vue
@@ -3,11 +3,53 @@
     <h2>Admin Table</h2>
     <div class="row">
       <h3 class="col-lg-6">Admin List</h3>
-      <button class="btn btn-success col-lg-6">Add +</button>
+     <!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg col-lg-6" data-bs-toggle="modal" data-bs-target="#myModal">Add City</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+                   <div class="modal-content">
+                          <div class="modal-header">
+                           
+                            <h4 class="modal-title"></h4>
+
+                            <h4 class="modal-title text-center">Add a new city</h4>
+                          </div>
+
+                          <div class="modal-body">
+                            <form @submit.prevent="createCity">
+                           <div class="row">
+
+                            <div class="form-group col-lg-12 col-sm-3 col-xs-6">
+                                <h5>Add a city</h5>
+                                <input type="text" name="city" id="city" v-model="city" placeholder="add City...">
+                              </div>
+
+                              <div class="form-group col-lg-12 col-sm-3 col-xs-6">
+                                <h5>Add an image</h5>
+                                <input type="text" name="image" v-model="img"  id="image" placeholder="add Img Url...">
+                              </div>
+                              <div class="form-group col-lg-12 col-sm-3 col-xs-6">
+                                <h5>Add an desc</h5>
+                                <input type="text" name="image" v-model="description"  id="image" placeholder="add desc...">
+                              </div>
+                          </div>
+                          <button type="submit">add City</button>
+                          </form>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
     </div>
 
     <div>
-      <table class="table table table-success table-striped">
+      <table class="table table table-success table-striped" v-for="city of cities" :key="city">
         <thead>
           <tr>
             <th scope="col">ID</th>
@@ -19,23 +61,32 @@
           </tr>
         </thead>
         <tbody>
-          {{cities}}
           <tr >
             <th scope="row">1</th>
-            <td>Drol</td>
-            <td>Otto</td>
+            <td>{{city.id}}</td>
+            <td>{{city.city}}</td>
+            <td>{{city.img}}</td>
+            <td>{{city.description}}</td>
             <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
+            <td><button @click="deleteCity(city.id)">delete</button></td>
           </tr>
          
         </tbody>
       </table>
     </div>
-  </div>
-</template>
+    </div>
+    </div>
+    </template>
 <script>
 export default {
+  data(){
+    return{
+      id:"",
+      city: "",
+      img: "",
+      description:"",
+    }
+  },
   mounted() {
     this.$store.dispatch("getCities");
   },
@@ -44,6 +95,22 @@ export default {
       return this.$store.state.cities;
     },
   },
+  methods:{
+    createCity(){
+      return this.$store.dispatch("createCity",{
+      city: this.city,
+      img: this.img,
+      description:this.description,
+      })
+    },
+    deleteCity(id){
+      return this.$store.dispatch("deleteCity", id)
+    }
+  }
 };
 </script>
-<style></style>
+<style>
+#admin-table{
+  background-color: rgb(255, 255, 255);
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -40,8 +40,9 @@ export default createStore({
       fetch("http://localhost:3000/cities/" + id, {
         method: "PUT",
         body: JSON.stringify({
-          title: "foo",
-          body: "bar",
+          city: city,
+          img: img,
+          description:description
         }),
         headers: {
           "Content-type": "application/json; charset=UTF-8",
@@ -51,25 +52,27 @@ export default createStore({
         .then((json) => console.log(json));
     },
 
-    createCity: async(context, city) => {
+    createCity: async(context, cities) => {
+      const {city,img ,description} = cities
       fetch('http://localhost:3000/cities', {
         method: 'POST',
         body: JSON.stringify({
-          title: 'foo',
-          body: 'bar',
+          city: city,
+          img: img,
+          description:description
         }),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
       })
         .then((response) => response.json())
-        .then((json) => console.log(json));
+        .then(() => context.dispatch("getCities"));
     },
 
     deleteCity: async(context, id) =>  {
-      fetch('http://localhost:3000/cities' + id, {
+      fetch('http://localhost:3000/cities/' + id, {
         method: 'DELETE',
-      });
+      }).then(() => context.dispatch("getCities"));
     },
 
     getAdmin:async(context, ) => {
